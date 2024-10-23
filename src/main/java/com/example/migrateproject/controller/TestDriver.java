9b64836761ea96dd37608e0358947376b5628725
thehieu03 <153330033+thehieu03@users.noreply.github.com>
2024-10-24 05:42:10 +0700

    Driver Test thanh cong

---
+++ src/main/java/com/example/migrateproject/controller/TestDriver.java
@@ -0,0 +1,25 @@
+package com.example.migrateproject.controller;import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "TestDriverServlet", value = "/TestDriver-servlet")
+public class TestDriver extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
